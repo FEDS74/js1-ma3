330dc98dde03ba15c5dc5da7bb9708ef3448cbb0
Thomas Bach-Nilsen <55656461+FEDS74@users.noreply.github.com>
2020-02-27 16:49:47 +0100

    Add files via upload
    t

---
+++ thomas-bach-nilsen-js1-ma3.js
@@ -0,0 +1,21 @@
+// QUESTION: 1
+(a, b) => a - b;
+
+
+// QUESTION: 2
+const apiRawgUrl = "https://api.rawg.io/api/games?genres=sports";
+const gamesUrl = `${apiRawgUrl}games`;
+
+// QUESTION: 3
+
+
+// QUESTION: 4
+
+
+// QUESTION: 5
+
+
+// QUESTION: 6
+
+
+// QUESTION: 7
